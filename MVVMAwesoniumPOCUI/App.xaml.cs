ed3041c774b51d4743c0f205f7e6607b4e8c0456
David Desmaisons <desmaisons_david@hotmail.com>
2014-09-04 12:40:09 -0300

    Refactoring to Core + Test + UI

--- MVVMAwesoniumPOCUI/App.xaml.cs
+++ MVVMAwesoniumPOCUI/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace MVVMAwesoniumPOC
+namespace MVVMAwesoniumPOCUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
