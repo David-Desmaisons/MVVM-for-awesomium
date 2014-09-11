b395bacce8fe256f5b04c03caa88a6089e4e9139
David Desmaisons <desmaisons_david@hotmail.com>
2014-09-11 20:46:08 -0300

    jsbasic added

---
+++ MVVMAwesoniumPOC/AwesomiumBinding/JavascriptToCSharp/JSBasicObject.cs
@@ -0,0 +1,27 @@
+﻿using Awesomium.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVVMAwesonium.AwesomiumBinding
+{
+    internal class JSBasicObject : IJSCBridge
+    {
+        public JSBasicObject(JSValue value, object icValue)
+        {
+            JSValue = value;
+            CValue = icValue;
+        }
+        public JSValue JSValue { get; set; }
+
+        public object CValue { get; set; }
+
+        public IEnumerable<IJSCBridge> GetChildren()
+        {
+            return Enumerable.Empty<IJSCBridge>();
+        }
+
+        public JSType Type { get { return JSType.Basic; } }
+    }
+}
