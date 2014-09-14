eea549d2fbc31202e985891d1189f2ea5f37a134
David Desmaisons <desmaisons_david@hotmail.com>
2014-09-14 13:35:52 -0300

    Chutzpah integration

--- MVVMAwesoniumPOC.Test/javascript/spec/MapToObservable.js
+++ MVVMAwesoniumPOC.Test/javascript/spec/MapToObservable.js
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/// <reference path="../../../MVVMAwesoniumPOC/Javascript/knockout.js" />
+/// <reference path="../../../MVVMAwesoniumPOC/Javascript//Ko_Extension.js" />
+
 
 describe("MapToObservable", function () {
     var basicmaped = { Name: "Albert", LastName: "Einstein" };
