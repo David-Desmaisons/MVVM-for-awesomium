c8dcde174272a2fedce50a2813c0ce8c01767b64
David Desmaisons <desmaisons_david@hotmail.com>
2014-09-18 00:19:47 -0300

    Datepicker UI

---
+++ MVVMAwesomium.UI.Calendar/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Awesomium.Core;
+using MVVMAwesonium.AwesomiumBinding;
+using MVVMAwesonium.ViewModel.Example;
+using MVVMAwesonium.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MVVMAwesomium.ViewModel.Example;
+
+namespace MVVMAwesomium.UI.Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WebControl.Source = new Uri(string.Format("{0}\\HTLM\\index.html", Assembly.GetExecutingAssembly().GetPath()));
+ 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IWebView f = this.WebControl.WebSession.Views.FirstOrDefault();
+            var datacontext = new DateInformation();
+            
+            AwesomeBinding.Bind(f, datacontext, JavascriptBindingMode.TwoWay);
+
+            Window w = sender as Window;
+            w.DataContext = datacontext;
+        }
+    }
+}
