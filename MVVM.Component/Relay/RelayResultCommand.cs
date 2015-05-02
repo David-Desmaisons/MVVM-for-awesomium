299e82596a90258bcc63758484a3296de9a3e293
David Desmaisons <desmaisons_david@hotmail.com>
2015-05-01 23:27:58 -0300

    Introduce new cpmmand pattern + move collectiontest folder

---
+++ MVVM.Component/Relay/RelayResultCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Component
+{
+    public class RelayResultCommand<Tin, TResult> : IResultCommand where Tin : class
+    {
+        private Func<Tin,TResult> _Function;
+        public RelayResultCommand(Func<Tin, TResult> iFunction)
+        {
+            _Function = iFunction;
+        }
+        public object Execute(object iargument)
+        {
+            return _Function(iargument as Tin);
+        }
+    }
+
+    public class RelayResultCommand<TResult> : IResultCommand
+    {
+        private Func< TResult> _Function;
+        public RelayResultCommand(Func<TResult> iFunction)
+        {
+            _Function = iFunction;
+        }
+        public object Execute(object iargument)
+        {
+            return _Function();
+        }
+    }
+}
---
+++ Tests/MVVM.Component.Relay.Test/RelaySimpleCommandTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSubstitute;
+using Xunit;
+using FluentAssertions;
+
+namespace MVVMAwesomium.Test
+{
+    public class RelaySimpleCommandTest
+    {
+        [Fact]
+        public void RelaySimpleCommandShouldCallActionGeneric()
+        {
+            var action = Substitute.For<Action<object>>();
+            var target = new MVVM.Component.RelaySimpleCommand<object>(action);
+            var arg = new object();
+
+            target.Execute(arg);
+
+            action.Received(1).Invoke(arg);
+        }
+
+        [Fact]
+        public void RelaySimpleCommandShouldCallAction()
+        {
+            var action = Substitute.For<Action>();
+            var target = new MVVM.Component.RelaySimpleCommand(action);
+            var arg = new object();
+
+            target.Execute(arg);
+
+            action.Received(1).Invoke();
+        }
+
+        [Fact]
+        public void RelayResultCommandShouldCallFunctionGeneric()
+        {
+            var function = Substitute.For<Func<object,int>>();
+            var arg = new object();
+            function.Invoke(arg).Returns(122);
+            var target = new MVVM.Component.RelayResultCommand<object,int>(function);
+      
+            var res = target.Execute(arg);
+
+            function.Received(1).Invoke(arg);
+            res.Should().Be(122);
+        }
+
+        [Fact]
+        public void RelayResultCommandShouldCallFunction()
+        {
+            var function = Substitute.For<Func<int>>();
+            function.Invoke().Returns(12);
+            var target = new MVVM.Component.RelayResultCommand<int>(function);
+            var arg = new object();
+
+            var res = target.Execute(arg);
+
+            function.Received(1).Invoke();
+            res.Should().Be(12);
+        }
+    }
+}
